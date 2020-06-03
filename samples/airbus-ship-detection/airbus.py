e7783f92d1568cb7cf96f53b8dee862a96764e42
Stefan <aegiskelle@gmail.com>
2020-06-03 01:13:30 +0000

    training airbus

--- samples/airbus-ship-detection/airbus.py
+++ samples/airbus-ship-detection/airbus.py
@@ -130,9 +130,13 @@ class AirbusDataset(utils.Dataset):
     
         # Add images
         iimageCount = 0
+        exclude_list = ['6384c3e78.jpg','13703f040.jpg', '14715c06d.jpg',  '33e0ff2d5.jpg','4d4e09f2a.jpg', '877691df8.jpg', '8b909bb20.jpg', 'a8d99130e.jpg', 'ad55c3143.jpg', 'c8260c541.jpg', 'd6c7f17c7.jpg', 'dc3e7c901.jpg','e44dffe88.jpg', 'ef87bad36.jpg', 'f083256d8.jpg'] #corrupted images
+
         for image_name in image_names: 
             if (iimageCount >=40000):
                 break
+            if (image_name in exclude_list):
+                continue
             image_annotation = ship_anns.query('ImageId=="' + image_name + '"')['EncodedPixels'].tolist()
             image_path = os.path.join(subset_datadir, image_name)            
             if ( (subset == "train" and iimageCount < 38000) or (subset == "val" and iimageCount >= 38000) ):
